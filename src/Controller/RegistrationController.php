c481b4c81644710ed7c4ec9fc3f62d876a56c175
GUILLEMIN Matheo <mguillemin@m-mguillemin.local>
2023-09-19 18:03:33 +0200

    register fait il faut juste rediriger vers le login fait par jean

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -38,7 +38,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_first');
+            return $this->redirectToRoute('app_register');
         }
 
         return $this->render('registration/register.html.twig', [
