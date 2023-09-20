e751b12548efb26e5394fe9396cbb352bade9c2f
kvong92 <vongkevin.c92@gmail.com>
2023-09-21 00:26:44 +0200

    setup homepage / blog homepage/ admin homepage

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomepageController extends AbstractController
+class AdminController extends AbstractController
 {
-    #[Route('/homepage', name: 'app_homepage')]
+    #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
         ]);
     }
 }
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomepageController extends AbstractController
+class BlogController extends AbstractController
 {
-    #[Route('/homepage', name: 'app_homepage')]
+    #[Route('/blog', name: 'app_blog')]
     public function index(): Response
     {
-
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
         ]);
     }
 }
