0525ed24047ddce909aaa473f81393a6d78a6e4f
kvong92 <vongkevin.c92@gmail.com>
2023-09-21 20:45:08 +0200

    update:routes

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,9 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+    public function adminDashboard(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        // or add an optional message - seen by developers
+        $this->denyAccessUnlessGranted(
+            'ROLE_ADMIN',
+            null,
+            'User tried to access a page without having ROLE_ADMIN');
+
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
+        $this->adminDashboard();
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
